a554050a4c2cb027892f40bd3a2b79fe7215c5e5
Pelle Braendgaard <pelleb@gmail.com>
2009-12-29 16:27:51 -0500

    moved neo4j to real dependencies from dev dependencies added clojars leiningen plugin

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject clojure-neo4j "0.1.0-SNAPSHOT" 
   :description "The clojure-neo4j project provides a more lispy interface to Neo4j, a graph-structured on-disk transactional database." 
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"] 
-                [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+                [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+                [org.neo4j/neo "1.0-b11"]
+                [org.neo4j/index-util "0.9"]]
   :repositories [["org.neo4j" "http://m2.neo4j.org"]]
-  :dev-dependencies   [[org.neo4j/neo "1.0-b11"]
-                      [org.neo4j/index-util "0.9"]])
+  :dev-dependencies   [[lein-clojars "0.5.0-SNAPSHOT"]])
