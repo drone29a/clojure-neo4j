3bcbb8d61a81279925feff4aafd8c4b9e47c3f2b
Pelle Braendgaard <pelleb@gmail.com>
2009-12-29 16:54:14 -0500

    Best to release 0.1.0 as mattrepl's original code to differentiate for future changes.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojure-neo4j "0.1.0-SNAPSHOT" 
+(defproject clojure-neo4j "0.1.0" 
   :description "The clojure-neo4j project provides a more lispy interface to Neo4j, a graph-structured on-disk transactional database." 
   :dependencies [[org.clojure/clojure "1.1.0-master-SNAPSHOT"] 
                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
