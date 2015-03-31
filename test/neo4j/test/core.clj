40a30a479271cb0772da229f64c69a0fe545505c
Matt Revelle <mrevelle@gmail.com>
2015-03-31 19:08:17 -0400

    Updated for neo4j 2.2.0.

--- test/neo4j/test/core.clj
+++ test/neo4j/test/core.clj
@@ -10,41 +10,41 @@
 
 (deftest new-node
   (let [db (neo4j/open (tmp-db-path "new-nodes"))
-        node (neo4j/with-tx db (neo4j/new-node db {"name" "foo"}))
-        labeled-node (neo4j/with-tx db (neo4j/new-node db "le label" {"name" "bar"}))]
+        node (neo4j/with-tx db (neo4j/new-node! db {"name" "foo"}))
+        labeled-node (neo4j/with-tx db (neo4j/new-node! db "le label" {"name" "bar"}))]
     (neo4j/with-tx db
       (is (= "foo" (neo4j/property (.getNodeById db (.getId node)) "name")))
       (is (= "bar" (neo4j/property (.getNodeById db (.getId labeled-node)) "name")))
       (is (contains? (set (neo4j/labels (.getNodeById db (.getId labeled-node)))) (neo4j/new-label "le label"))))
-    (neo4j/shutdown db)))
+    (neo4j/shutdown! db)))
 
 (deftest relate
   (let [db (neo4j/open (tmp-db-path "relate"))
-        src (neo4j/with-tx db (neo4j/new-node db {"name" "foo"}))
-        dst (neo4j/with-tx db (neo4j/new-node db {"name" "bar"}))
-        rel (neo4j/with-tx db (neo4j/relate src :likes dst))]
+        src (neo4j/with-tx db (neo4j/new-node! db {"name" "foo"}))
+        dst (neo4j/with-tx db (neo4j/new-node! db {"name" "bar"}))
+        rel (neo4j/with-tx db (neo4j/relate! :likes src dst))]
     (neo4j/with-tx db
       (is (= "likes" (-> (.getRelationshipById db (.getId rel))
                        (.getType)
                        (.name))))
       (is (= src (.getStartNode (.getRelationshipById db (.getId rel)))))
       (is (= dst (.getEndNode (.getRelationshipById db (.getId rel))))))
-    (neo4j/shutdown db)))
+    (neo4j/shutdown! db)))
 
 (deftest traverse
   (let [db (neo4j/open (tmp-db-path "traverse"))
-        user-root (neo4j/with-tx db (neo4j/new-node db {"name" "user-root"}))
-        alice (neo4j/with-tx db (neo4j/new-node db {"name" "alice"}))
-        bob (neo4j/with-tx db (neo4j/new-node db {"name" "bob"}))
-        carol (neo4j/with-tx db (neo4j/new-node db {"name" "carol"}))]
+        user-root (neo4j/with-tx db (neo4j/new-node! db {"name" "user-root"}))
+        alice (neo4j/with-tx db (neo4j/new-node! db {"name" "alice"}))
+        bob (neo4j/with-tx db (neo4j/new-node! db {"name" "bob"}))
+        carol (neo4j/with-tx db (neo4j/new-node! db {"name" "carol"}))]
     (neo4j/with-tx db
-      (neo4j/relate user-root :user alice)
-      (neo4j/relate user-root :user bob)
-      (neo4j/relate user-root :user carol)
+      (neo4j/relate! :user user-root alice)
+      (neo4j/relate! :user user-root bob)
+      (neo4j/relate! :user user-root carol)
       
-      (neo4j/relate alice :likes bob)
-      (neo4j/relate bob :likes carol)
-      (neo4j/relate carol :likes alice))
+      (neo4j/relate! :likes alice bob)
+      (neo4j/relate! :likes bob carol)
+      (neo4j/relate! :likes carol alice))
 
     (neo4j/with-tx db
       (is (= 3 (count (neo4j/traverse user-root
@@ -69,46 +69,46 @@
                                                                neo4j/all-but-start
                                                                {:likes neo4j/outgoing})))))))
   
-    (neo4j/shutdown db)))
+    (neo4j/shutdown! db)))
 
 (deftest global-ops
   (let [db (neo4j/open (tmp-db-path "global-ops"))
-        user-root (neo4j/with-tx db (neo4j/new-node db {"name" "user-root"}))
-        alice (neo4j/with-tx db (neo4j/new-node db {"name" "alice"}))
-        bob (neo4j/with-tx db (neo4j/new-node db {"name" "bob"}))
-        carol (neo4j/with-tx db (neo4j/new-node db {"name" "carol"}))]
+        user-root (neo4j/with-tx db (neo4j/new-node! db {"name" "user-root"}))
+        alice (neo4j/with-tx db (neo4j/new-node! db {"name" "alice"}))
+        bob (neo4j/with-tx db (neo4j/new-node! db {"name" "bob"}))
+        carol (neo4j/with-tx db (neo4j/new-node! db {"name" "carol"}))]
     (neo4j/with-tx db
-      (neo4j/relate user-root :user alice)
-      (neo4j/relate user-root :user bob)
-      (neo4j/relate user-root :user carol)
+      (neo4j/relate! :user user-root alice)
+      (neo4j/relate! :user user-root bob)
+      (neo4j/relate! :user user-root carol)
       
-      (neo4j/relate alice :likes bob)
-      (neo4j/relate bob :likes carol)
-      (neo4j/relate carol :likes alice))
+      (neo4j/relate! :likes alice bob)
+      (neo4j/relate! :likes bob carol)
+      (neo4j/relate! :likes carol alice))
 
     (neo4j/with-tx db
       (is (= 4 (count (neo4j/all-nodes db))))
       (is (= 6 (count (neo4j/all-relationships db)))))
   
-    (neo4j/shutdown db)))
+    (neo4j/shutdown! db)))
 
 (deftest properties
   (let [db (neo4j/open (tmp-db-path "properties"))
-        node (neo4j/with-tx db (neo4j/new-node db {"name" "foo"}))]
+        node (neo4j/with-tx db (neo4j/new-node! db {"name" "foo"}))]
     (neo4j/with-tx db
       (is (nil? (neo4j/property node "dne"))))
-    (neo4j/shutdown db)))
+    (neo4j/shutdown! db)))
 
 (deftest indexing
   (let [db (neo4j/open (tmp-db-path "indexing"))]
     (neo4j/with-tx db
-      (neo4j/new-index db "a-label" :a))
+      (neo4j/new-index! db "a-label" :a))
     (neo4j/with-tx db
-      (neo4j/new-node db "a-label" {:a "abc"
+      (neo4j/new-node! db "a-label" {:a "abc"
                                     :b 123})
-      (neo4j/new-node db "a-label" {:a "abc"
+      (neo4j/new-node! db "a-label" {:a "abc"
                                     :d 456})
-      (neo4j/new-node db "other-label" {:e "aiz"
+      (neo4j/new-node! db "other-label" {:e "aiz"
                                         :f 136})
       (is (= 1 (count (neo4j/all-indexes db "a-label"))))
       (is (= 2 (count (neo4j/all-nodes db "a-label" :a "abc"))))
